1718b1c8f17f05b3c3fc04df9316f7c751d6626e
iyustlop <iyustlop@gmail.com>
2017-01-09 18:45:26 +0100

    startDB create DataBase and table.person

--- initdb.cpp
+++ initdb.cpp
@@ -6,5 +6,16 @@ initDB::initDB()
 }
 
 QSqlError initDB::startDb(){
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("C:\\Users\\iyust\\Documents\\Desarrollo\\valvulares.db");
+
+    if (!db.open())
+        return db.lastError();
+
+    QSqlQuery query;
+    if(!query.exec("create table person (id int primary key, "
+                   "firstname varchar(20), lastname varchar(20))"))
+            return query.lastError();
+
     return QSqlError();
 }
