bf5ee1dd439d4f19d6733fdbd527750958602966
iyustlop <iyustlop@gmail.com>
2017-10-25 00:20:39 +0200

    refinement to include new location

--- db/initdbcita.h
+++ db/initdbcita.h
@@ -1,14 +1,14 @@
 #ifndef INITDBCITA_H
 #define INITDBCITA_H
-#include "cita.h"
-
+#include "cita/cita.h"
+#include "cita/parametrosanaliticos.h"
 #include <QtSql>
 
 class initDBCita
 {
 public:
     initDBCita();
-    QSqlError insertCita(Cita cita);
+    QSqlError insertCita(Cita cita, ParametrosAnaliticos parametrosAnaliticos);
 };
 
 #endif // INITDBCITA_H
