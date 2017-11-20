84e1327c6f1ffd0a884ebe5bbfda2534873cb809
iyustlop <iyustlop@gmail.com>
2017-11-21 00:13:38 +0100

    Added cita and ParametrosAnaliticos

--- db/initdbcita.cpp
+++ db/initdbcita.cpp
@@ -6,9 +6,37 @@ initDBCita::initDBCita()
 
 }
 
-QSqlError initDBCita::insertCita(QString numeroHistoria, visit visita){
-//    QString numeroHistoria = MainWindow::getNumeroHistoria();
+QSqlError initDBCita::insertCita(QString nHistoria, visit visita){
+    QString numeroHistoria;
+    Cita cita;
+    ParametrosAnaliticos parametrosAnaliticos;
 
+    numeroHistoria = nHistoria;
+    cita = visita.getCita();
+    parametrosAnaliticos = visita.getParametrosAnaliticos();
 
-return QSqlError();
+    QSqlQuery query;
+
+    if (!query.prepare("insert into cita values(:visitId,:dateVisit,:rhythm,:functionalGrade,:FRCV)"))
+        return query.lastError();
+    query.bindValue(":visitId",numeroHistoria.toInt());
+    query.bindValue(":dateVisit",cita.getFechaConsulta());
+    query.bindValue(":rhythm",cita.getRitmo());
+    query.bindValue(":functionalGrade",cita.getGradoFuncional());
+    query.bindValue(":FRCV",cita.getFrcv());
+    if (!query.exec())
+        return query.lastError();
+
+    if (!query.prepare("insert into parametrosAnaliticos values(:analiticalParameter,:HB,:creatinina,:FG,:proBNP,:potasio)"))
+        return query.lastError();
+    query.bindValue(":analiticalParameter",numeroHistoria.toInt());
+    query.bindValue(":HB",parametrosAnaliticos.getHB());
+    query.bindValue(":creatinina",parametrosAnaliticos.getCreatinina());
+    query.bindValue(":FG",parametrosAnaliticos.getFG());
+    query.bindValue(":proBNP",parametrosAnaliticos.getProBNP());
+    query.bindValue(":potasio",parametrosAnaliticos.getPotasio());
+    if (!query.exec())
+        return query.lastError();
+
+    return QSqlError();
 }
