3c9729a3d4e19763a3dd529457b39a156d24361c
iyustlop <iyustlop@gmail.com>
2017-11-28 20:11:55 +0100

    Refinement in creation of table to include index autoincrement

--- db/initdbcita.cpp
+++ db/initdbcita.cpp
@@ -17,9 +17,9 @@ QSqlError initDBCita::insertCita(QString nHistoria, visit visita){
 
     QSqlQuery query;
 
-    if (!query.prepare("insert into cita values(:visitId,:dateVisit,:rhythm,:functionalGrade,:FRCV)"))
+    if (!query.prepare("insert into cita (personId,dateVisit,rhythm,functionalGrade,FRCV) values (:personId,:dateVisit,:rhythm,:functionalGrade,:FRCV)"))
         return query.lastError();
-    query.bindValue(":visitId",numeroHistoria.toInt());
+    query.bindValue(":personId",numeroHistoria.toInt());
     query.bindValue(":dateVisit",cita.getFechaConsulta());
     query.bindValue(":rhythm",cita.getRitmo());
     query.bindValue(":functionalGrade",cita.getGradoFuncional());
